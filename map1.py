b3a426e9b998423984b1bc5b9def18302a65647d
Shiwangsh KC <Shiwangshkc@gmail.com>
2024-05-06 11:53:28 +0545

    Add world data

--- map1.py
+++ map1.py
@@ -5,19 +5,19 @@ data = pd.read_csv('volcano_db.csv', encoding='latin1')
 
 philippines_volcanoes = data[data['Country'] == 'Philippines']
 
-lat = list(philippines_volcanoes["Latitude"])
-lon = list(philippines_volcanoes["Longitude"])
-elev = list(philippines_volcanoes["Elev"])
-country = list(philippines_volcanoes["Country"])
-status = list(philippines_volcanoes["Status"])
-name = list(philippines_volcanoes["Volcano Name"])
+# lat = list(philippines_volcanoes["Latitude"])
+# lon = list(philippines_volcanoes["Longitude"])
+# elev = list(philippines_volcanoes["Elev"])
+# country = list(philippines_volcanoes["Country"])
+# status = list(philippines_volcanoes["Status"])
+# name = list(philippines_volcanoes["Volcano Name"])
 
-# lat = list(data["Latitude"])
-# lon = list(data["Longitude"])
-# elev = list(data["Elev"])
-# country = list(data["Country"])
-# status = list(data["Status"])
-# name = list(data["Volcano Name"])
+lat = list(data["Latitude"])
+lon = list(data["Longitude"])
+elev = list(data["Elev"])
+country = list(data["Country"])
+status = list(data["Status"])
+name = list(data["Volcano Name"])
 
 
 
@@ -43,16 +43,44 @@ for lt,ln,el,name ,status in zip(lat,lon,elev,name,status):
     """
     fgVolcanoes.add_child(folium.CircleMarker(location=[lt,ln], popup=folium.Popup(popup_content, max_width=300),radius = 6, fill_color=color_producer(el),fill_opacity=1, color='grey'))
 
+def style_function(x):
+    return {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}
 
+
+# popup = folium.GeoJsonPopup(
+#         fields=["NAME", "POP2005"],
+#         aliases=["Country", "Population"],
+#         localize=True,
+#         labels=True,
+#         style="background-color: yellow;",
+#     )
+
+# tooltip = folium.GeoJsonTooltip(
+#     fields=["NAME", "POP2005" ],
+#     aliases=["Name:", "population 2005:"],
+#     localize=True,
+#     sticky=False,
+#     labels=True,
+#     style="""
+#         background-color: #F0EFEF;
+#         border: 2px solid black;
+#         border-radius: 3px;
+#         box-shadow: 3px;
+#     """,
+#     max_width=800,
+# )
+
+def highlight_function(x):
+    return {'fillColor': 'yellow', 'color': 'blue', 'fillOpacity': 0.5, 'weight': 2.5}
 
 
 fgPopulation= folium.FeatureGroup(name="Population")
+fgPopulation.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),style_function=style_function, highlight_function=highlight_function))
 
 
-fgPopulation.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
+map.add_child(fgPopulation)
 map.add_child(fgVolcanoes)
-# map.add_child(fgPopulation)
 
 map.add_child(folium.LayerControl())
 
